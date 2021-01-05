a20b7fe674aa3d71d6143ed92c0ba34a775fe53d
Elian Valdez <elian.valdez09@gmail.com>
2021-01-05 15:25:27 -0300

    1006

---
+++ 1006.py
@@ -0,0 +1,7 @@
+firstValue = float(input())
+secondValue = float(input())
+thirdValue = float(input())
+
+answer = (firstValue * 2 + secondValue * 3 + thirdValue * 5) / 10
+
+print("MEDIA = {:.1f}".format(answer))
