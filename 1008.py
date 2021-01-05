d49ae7d896e69c9bb2f1c063223cdd783333f62f
Elian Valdez <elian.valdez09@gmail.com>
2021-01-05 15:27:50 -0300

    1008

---
+++ 1008.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+NUMBER = int(input())
+B = int(input())
+C = float(input())
+
+SALARY = B*C
+
+print("NUMBER = {}".format(NUMBER))
+print("SALARY = U$ {:.2f}".format(SALARY))
